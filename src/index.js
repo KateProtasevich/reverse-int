5ee9064e73ef799ba374f1127aeb1c391dfceddf
KateProtasevich <KrishniovaKate@mail.ru>
2020-03-02 16:14:52 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+  let number = Math.abs(n);
+  for (let i = number; i > 0; i = Math.trunc(i/10)) {
+    result = result*10 + i%10;
+    console.log(result);
+  }
+  return result;
 }
